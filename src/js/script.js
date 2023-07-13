cf174e9319254b02382bfa12d94ed2f4282bb038
Zayats Kiryl <zayatsk65@gmail.com>
2023-07-13 15:30:39 +0300

    feture(pintrest): add header

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+
+// let dropdown = document.getElementById("dropdownMenuButton");
+// dropdown.addEventListener("click", () => {
+//     let list = dropdown.parentElement.getElementsByClassName("dropdown-menu")[0];
+//     list.style.display = 'block';
+//     list.style.right = '1px';
+// });
