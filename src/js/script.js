6514ede97491a62134dfaa007b9f4aa5bb0edf8e
Кирилл Заяц <kzayats@gis365by.onmicrosoft.com>
2023-07-14 13:50:10 +0300

    feature(pintrest): add grid

--- src/js/script.js
+++
@@ -1,7 +0,0 @@
-
-// let dropdown = document.getElementById("dropdownMenuButton");
-// dropdown.addEventListener("click", () => {
-//     let list = dropdown.parentElement.getElementsByClassName("dropdown-menu")[0];
-//     list.style.display = 'block';
-//     list.style.right = '1px';
-// });
