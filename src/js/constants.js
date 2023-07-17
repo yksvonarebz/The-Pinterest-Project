6d988a43bc9a1376f9362375bc9acc6a1520f8e1
Кирилл Заяц <kzayats@gis365by.onmicrosoft.com>
2023-07-18 01:41:37 +0300

    feature(pintrest): add scaling and user info

---
+++ src/js/constants.js
@@ -0,0 +1,30 @@
+// Period of repeat height
+const PERIODIC_NUM = 16;
+
+// Map of heights
+const HEIGHT_MAP = new Map([
+  [0, "295px"],
+  [1, "378px"],
+  [2, "315px"],
+  [3, "420px"],
+  [4, "429px"],
+  [5, "315px"],
+  [6, "229px"],
+  [7, "420px"],
+  [8, "472px"],
+  [9, "347px"],
+  [10, "268px"],
+  [11, "345px"],
+  [12, "418px"],
+  [13, "354px"],
+  [14, "313px"],
+  [15, "420px"],
+]);
+
+const XL_WIN = 1300;
+const L_WIN = 1000;
+const M_WIN = 800;
+const S_WIN = 500;
+const XS_WIN = 400;
+
+export { PERIODIC_NUM, HEIGHT_MAP, XL_WIN, L_WIN, M_WIN, S_WIN, XS_WIN };
